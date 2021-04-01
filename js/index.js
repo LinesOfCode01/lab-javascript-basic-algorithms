d7b8b9abab6d597d667277913d8a740b907db2bf
linesofcode01 <frcoding@outlook.com>
2021-03-31 22:47:28 -0700

    basicAlgoUpdate

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-// Iteration 1: Names and Input
-
-
-// Iteration 2: Conditionals
-
+      // Iteration 1: Names and Input
+      let hacker1 = "driverName"
+      console.log(`The driver's name is ${hacker1}`)
+      
+      let hacker2 = "navigatorName"
+      console.log(`The navigator's name is ${hacker2}`)       
+      
+      // Iteration 2: Conditionals
+      
+      function longestName(hacker1, hacker2){
+          if(hacker1.length > hacker2.length){
+              return `${hacker1} has the longest name, it has ${hacker1.length}`
+          } else if (hacker1.length === hacker2.length){
+              return `Wow, you both have equally long names, ${hacker1} characters!`
+             
+          } else {
+              return `${hacker2} has the longest name, it has ${hacker2.length}`
+          
+          }
+      }
+      
+      console.log(longestName(hacker1,hacker2))
 
 // Iteration 3: Loops
+
+
